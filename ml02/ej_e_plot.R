dd62beba099635809df02186a0f204dadbc703d1
Hernán Gurmendi <hgurmen@gmail.com>
2018-05-26 18:43:39 -0300

    Preliminary version of exercise e plotter

---
+++ ml02/ej_e_plot.R
@@ -0,0 +1,74 @@
+#!/usr/bin/Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 3) {
+    message("USAGE: ./ej07_plot.R parallel_errors diagonal_errors anns_errors")
+    quit()
+}
+
+message("parallel")
+message(args[1])
+
+message("diagonal")
+message(args[2])
+
+parallel <- read.csv(args[1], header = TRUE)
+diagonal <- read.csv(args[2], header = TRUE)
+anns <- read.csv(args[3], header = FALSE)
+
+anns_diagonal <- subset(anns, V1=="diagonal")
+anns_parallel <- subset(anns, V1=="parallel")
+
+minX <- min(parallel$d, diagonal$d)
+maxX <- max(parallel$d, diagonal$d)
+
+minY <- min(parallel$TestEBP, parallel$TrainEBP, diagonal$TrainEBP, diagonal$TestEBP,
+            parallel$TestEAP, parallel$TrainEAP, diagonal$TrainEAP, diagonal$TestEAP,
+            anns_diagonal$V3, anns_parallel$V3)
+maxY <- max(parallel$TestEBP, parallel$TrainEBP, diagonal$TrainEBP, diagonal$TestEBP,
+            parallel$TestEAP, parallel$TrainEAP, diagonal$TrainEAP, diagonal$TestEAP,
+            anns_diagonal$V3, anns_parallel$V3)
+
+
+# rojo -> diagonal
+# verde -> parallel
+
+# red -> decision tree
+#   full width -> parallel
+#             -> diagonal
+# green -> anns
+
+png("ej_e.png")
+par(mar=c(4,4,1,1)) # par = parametros de plot, mar = margenes, c(bottom, left, top, right)
+plot(diagonal$d
+   , diagonal$TestEAP
+   , col = "red"
+   , type = "o"
+   , xlim = c(minX, maxX)
+   , ylim = c(minY, maxY)
+   , xlab = "Dimensions"
+   , ylab = "Error percentage"
+   , lwd = 2
+   , lty = 1)
+
+lines(parallel$d
+    , parallel$TestEAP
+    , col = "red"
+    , type = "o"
+    , lwd = 2
+    , lty = 2)
+
+lines(anns_parallel$V2
+    , anns_parallel$V3
+    , col = "green"
+    , type = "o"
+    , lwd = 2
+    , lty = 1)
+
+lines(anns_diagonal$V2
+    , anns_diagonal$V3
+    , col = "green"
+    , type = "o"
+    , lwd = 2
+    , lty = 2)

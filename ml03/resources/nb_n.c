01e0f0cc6a58d8eaa441105ca29fa4d633cb9f11
Hernán Gurmendi <hgurmen@gmail.com>
2018-06-04 16:22:51 -0300

    Replace tabs for spaces

--- ml03/resources/nb_n.c
+++ ml03/resources/nb_n.c
@@ -11,34 +11,34 @@ PMG - Ultima revision: 20/06/2001
 #include <stdlib.h>
 #include <time.h>
 
-#define LOW 1.e-14		/*Minimo valor posible para una probabilidad */
+#define LOW 1.e-14      /*Minimo valor posible para una probabilidad */
 #define PI  3.141592653
 
-int N_IN;			/*Total numbre of inputs */
-int N_Class;			/*Total number of classes (outputs) */
+int N_IN;               /*Total numbre of inputs */
+int N_Class;            /*Total number of classes (outputs) */
 
-int PTOT;			/* cantidad TOTAL de patrones en el archivo .data */
-int PR;				/* cantidad de patrones de ENTRENAMIENTO */
-int PTEST;			/* cantidad de patrones de TEST (archivo .test) */
-		    /* cantidad de patrones de VALIDACION: PTOT - PR */
+int PTOT;               /* cantidad TOTAL de patrones en el archivo .data */
+int PR;                 /* cantidad de patrones de ENTRENAMIENTO */
+int PTEST;              /* cantidad de patrones de TEST (archivo .test) */
+                        /* cantidad de patrones de VALIDACION: PTOT - PR */
 
-int SEED;			/* semilla para la funcion rand(). Los posibles valores son: */
-		    /* SEED: -1: No mezclar los patrones: usar los primeros PR para entrenar
-		       y el resto para validar.Toma la semilla del rand con el reloj.
-		       0: Seleccionar semilla con el reloj, y mezclar los patrones.
-		       >0: Usa el numero leido como semilla, y mezcla los patrones. */
+int SEED;               /* semilla para la funcion rand(). Los posibles valores son: */
+                        /* SEED: -1: No mezclar los patrones: usar los primeros PR para entrenar
+                           y el resto para validar.Toma la semilla del rand con el reloj.
+                           0: Seleccionar semilla con el reloj, y mezclar los patrones.
+                           >0: Usa el numero leido como semilla, y mezcla los patrones. */
 
-int CONTROL;			/* nivel de verbosity: 0 -> solo resumen, 1 -> 0 + pesos, 2 -> 1 + datos */
+int CONTROL;            /* nivel de verbosity: 0 -> solo resumen, 1 -> 0 + pesos, 2 -> 1 + datos */
 
-int N_TOTAL;			/*Numero de patrones a usar durante el entrenamiento */
+int N_TOTAL;            /*Numero de patrones a usar durante el entrenamiento */
 
 /*matrices globales  DECLARAR ACA LAS MATRICES NECESARIAS */
 
-double **data;			/* train data */
-double **test;			/* test  data */
-int *pred;			/* clases predichas */
+double **data;          /* train data */
+double **test;          /* test  data */
+int *pred;              /* clases predichas */
 
-int *seq;			/* sequencia de presentacion de los patrones */
+int *seq;               /* sequencia de presentacion de los patrones */
 
 /*variables globales auxiliares*/
 char filepat[100];
@@ -52,37 +52,37 @@ int error;
 int define_matrix()
 {
 
-	int i, max;
-	if (PTOT > PTEST)
-		max = PTOT;
-	else
-		max = PTEST;
+    int i, max;
+    if (PTOT > PTEST)
+        max = PTOT;
+    else
+        max = PTEST;
 
-	seq = (int *)calloc(max, sizeof(int));
-	pred = (int *)calloc(max, sizeof(int));
-	if (seq == NULL || pred == NULL)
-		return 1;
+    seq = (int *)calloc(max, sizeof(int));
+    pred = (int *)calloc(max, sizeof(int));
+    if (seq == NULL || pred == NULL)
+        return 1;
 
-	data = (double **)calloc(PTOT, sizeof(double *));
-	if (PTEST)
-		test = (double **)calloc(PTEST, sizeof(double *));
-	if (data == NULL || (PTEST && test == NULL))
-		return 1;
+    data = (double **)calloc(PTOT, sizeof(double *));
+    if (PTEST)
+        test = (double **)calloc(PTEST, sizeof(double *));
+    if (data == NULL || (PTEST && test == NULL))
+        return 1;
 
-	for (i = 0; i < PTOT; i++) {
-		data[i] = (double *)calloc(N_IN + 1, sizeof(double));
-		if (data[i] == NULL)
-			return 1;
-	}
-	for (i = 0; i < PTEST; i++) {
-		test[i] = (double *)calloc(N_IN + 1, sizeof(double));
-		if (test[i] == NULL)
-			return 1;
-	}
+    for (i = 0; i < PTOT; i++) {
+        data[i] = (double *)calloc(N_IN + 1, sizeof(double));
+        if (data[i] == NULL)
+            return 1;
+    }
+    for (i = 0; i < PTEST; i++) {
+        test[i] = (double *)calloc(N_IN + 1, sizeof(double));
+        if (test[i] == NULL)
+            return 1;
+    }
 
 /*ALLOCAR ESPACIO PARA LAS MATRICES DEL ALGORITMO*/
 
-	return 0;
+    return 0;
 }
 
 /* ---------------------------------------------------------------------------------- */
@@ -91,59 +91,59 @@ int define_matrix()
 /* ---------------------------------------------------------------------------------- */
 int arquitec(char *filename)
 {
-	FILE *b;
-	time_t t;
+    FILE *b;
+    time_t t;
 
-	/*Paso 1:leer el archivo con la configuracion */
-	sprintf(filepat, "%s.nb", filename);
-	b = fopen(filepat, "r");
-	error = (b == NULL);
-	if (error) {
-		printf("Error al abrir el archivo de parametros\n");
-		return 1;
-	}
+    /*Paso 1:leer el archivo con la configuracion */
+    sprintf(filepat, "%s.nb", filename);
+    b = fopen(filepat, "r");
+    error = (b == NULL);
+    if (error) {
+        printf("Error al abrir el archivo de parametros\n");
+        return 1;
+    }
 
-	/* Dimensiones */
-	fscanf(b, "%d", &N_IN);
-	fscanf(b, "%d", &N_Class);
+    /* Dimensiones */
+    fscanf(b, "%d", &N_IN);
+    fscanf(b, "%d", &N_Class);
 
-	/* Archivo de patrones: datos para train y para validacion */
-	fscanf(b, "%d", &PTOT);
-	fscanf(b, "%d", &PR);
-	fscanf(b, "%d", &PTEST);
+    /* Archivo de patrones: datos para train y para validacion */
+    fscanf(b, "%d", &PTOT);
+    fscanf(b, "%d", &PR);
+    fscanf(b, "%d", &PTEST);
 
-	/* Semilla para la funcion rand() */
-	fscanf(b, "%d", &SEED);
+    /* Semilla para la funcion rand() */
+    fscanf(b, "%d", &SEED);
 
-	/* Nivel de verbosity */
-	fscanf(b, "%d", &CONTROL);
+    /* Nivel de verbosity */
+    fscanf(b, "%d", &CONTROL);
 
-	fclose(b);
+    fclose(b);
 
-	/*Paso 2: Definir matrices para datos y parametros (e inicializarlos */
-	error = define_matrix();
-	if (error) {
-		printf("Error en la definicion de matrices\n");
-		return 1;
-	}
+    /*Paso 2: Definir matrices para datos y parametros (e inicializarlos */
+    error = define_matrix();
+    if (error) {
+        printf("Error en la definicion de matrices\n");
+        return 1;
+    }
 
-	/* Chequear semilla para la funcion rand() */
-	if (SEED == 0)
-		SEED = time(&t);
+    /* Chequear semilla para la funcion rand() */
+    if (SEED == 0)
+        SEED = time(&t);
 
-	/*Imprimir control por pantalla */
-	printf
-	    ("\nNaive Bayes con distribuciones normales:\nCantidad de entradas:%d",
-	     N_IN);
-	printf("\nCantidad de clases:%d", N_Class);
-	printf("\nArchivo de patrones: %s", filename);
-	printf("\nCantidad total de patrones: %d", PTOT);
-	printf("\nCantidad de patrones de entrenamiento: %d", PR);
-	printf("\nCantidad de patrones de validacion: %d", PTOT - PR);
-	printf("\nCantidad de patrones de test: %d", PTEST);
-	printf("\nSemilla para la funcion rand(): %d", SEED);
+    /*Imprimir control por pantalla */
+    printf
+        ("\nNaive Bayes con distribuciones normales:\nCantidad de entradas:%d",
+         N_IN);
+    printf("\nCantidad de clases:%d", N_Class);
+    printf("\nArchivo de patrones: %s", filename);
+    printf("\nCantidad total de patrones: %d", PTOT);
+    printf("\nCantidad de patrones de entrenamiento: %d", PR);
+    printf("\nCantidad de patrones de validacion: %d", PTOT - PR);
+    printf("\nCantidad de patrones de test: %d", PTEST);
+    printf("\nSemilla para la funcion rand(): %d", SEED);
 
-	return 0;
+    return 0;
 }
 
 /* -------------------------------------------------------------------------------------- */
@@ -155,66 +155,66 @@ int arquitec(char *filename)
 int read_data(char *filename)
 {
 
-	FILE *fpat;
-	double valor;
-	int i, k, separador;
+    FILE *fpat;
+    double valor;
+    int i, k, separador;
 
-	sprintf(filepat, "%s.data", filename);
-	fpat = fopen(filepat, "r");
-	error = (fpat == NULL);
-	if (error) {
-		printf("Error al abrir el archivo de datos\n");
-		return 1;
-	}
+    sprintf(filepat, "%s.data", filename);
+    fpat = fopen(filepat, "r");
+    error = (fpat == NULL);
+    if (error) {
+        printf("Error al abrir el archivo de datos\n");
+        return 1;
+    }
 
-	if (CONTROL > 1)
-		printf("\n\nDatos de entrenamiento:");
+    if (CONTROL > 1)
+        printf("\n\nDatos de entrenamiento:");
 
-	for (k = 0; k < PTOT; k++) {
-		if (CONTROL > 1)
-			printf("\nP%d:\t", k);
-		for (i = 0; i < N_IN + 1; i++) {
-			fscanf(fpat, "%lf", &valor);
-			data[k][i] = valor;
-			if (CONTROL > 1)
-				printf("%lf\t", data[k][i]);
-			separador = getc(fpat);
-			if (separador != ',')
-				ungetc(separador, fpat);
-		}
-	}
-	fclose(fpat);
+    for (k = 0; k < PTOT; k++) {
+        if (CONTROL > 1)
+            printf("\nP%d:\t", k);
+        for (i = 0; i < N_IN + 1; i++) {
+            fscanf(fpat, "%lf", &valor);
+            data[k][i] = valor;
+            if (CONTROL > 1)
+                printf("%lf\t", data[k][i]);
+            separador = getc(fpat);
+            if (separador != ',')
+                ungetc(separador, fpat);
+        }
+    }
+    fclose(fpat);
 
-	if (!PTEST)
-		return 0;
+    if (!PTEST)
+        return 0;
 
-	sprintf(filepat, "%s.test", filename);
-	fpat = fopen(filepat, "r");
-	error = (fpat == NULL);
-	if (error) {
-		printf("Error al abrir el archivo de test\n");
-		return 1;
-	}
+    sprintf(filepat, "%s.test", filename);
+    fpat = fopen(filepat, "r");
+    error = (fpat == NULL);
+    if (error) {
+        printf("Error al abrir el archivo de test\n");
+        return 1;
+    }
 
-	if (CONTROL > 1)
-		printf("\n\nDatos de test:");
+    if (CONTROL > 1)
+        printf("\n\nDatos de test:");
 
-	for (k = 0; k < PTEST; k++) {
-		if (CONTROL > 1)
-			printf("\nP%d:\t", k);
-		for (i = 0; i < N_IN + 1; i++) {
-			fscanf(fpat, "%lf", &valor);
-			test[k][i] = valor;
-			if (CONTROL > 1)
-				printf("%lf\t", test[k][i]);
-			separador = getc(fpat);
-			if (separador != ',')
-				ungetc(separador, fpat);
-		}
-	}
-	fclose(fpat);
+    for (k = 0; k < PTEST; k++) {
+        if (CONTROL > 1)
+            printf("\nP%d:\t", k);
+        for (i = 0; i < N_IN + 1; i++) {
+            fscanf(fpat, "%lf", &valor);
+            test[k][i] = valor;
+            if (CONTROL > 1)
+                printf("%lf\t", test[k][i]);
+            separador = getc(fpat);
+            if (separador != ',')
+                ungetc(separador, fpat);
+        }
+    }
+    fclose(fpat);
 
-	return 0;
+    return 0;
 
 }
 
@@ -226,25 +226,25 @@ int read_data(char *filename)
 /* ------------------------------------------------------------ */
 void shuffle(int hasta)
 {
-	double x;
-	int tmp;
-	int top, select;
+    double x;
+    int tmp;
+    int top, select;
 
-	top = hasta - 1;
-	while (top > 0) {
-		x = (double)rand();
-		x /= RAND_MAX;
-		x *= (top + 1);
-		select = (int)x;
-		tmp = seq[top];
-		seq[top] = seq[select];
-		seq[select] = tmp;
-		top--;
-	}
-	if (CONTROL > 3) {
-		printf("End shuffle\n");
-		fflush(NULL);
-	}
+    top = hasta - 1;
+    while (top > 0) {
+        x = (double)rand();
+        x /= RAND_MAX;
+        x *= (top + 1);
+        select = (int)x;
+        tmp = seq[top];
+        seq[top] = seq[select];
+        seq[select] = tmp;
+        top--;
+    }
+    if (CONTROL > 3) {
+        printf("End shuffle\n");
+        fflush(NULL);
+    }
 }
 
 /* ------------------------------------------------------------------- */
@@ -254,7 +254,7 @@ void shuffle(int hasta)
 double prob(double x, int feature, int clase)
 {
 
-	 /*IMPLEMENTAR*/ return prob;
+     /*IMPLEMENTAR*/ return prob;
 }
 
 /* ------------------------------------------------------------------------------ */
@@ -265,28 +265,28 @@ double prob(double x, int feature, int clase)
 int output(double *input)
 {
 
-	double prob_de_clase;
-	double max_prob = -1e40;
-	int i, k, clase_MAP;
+    double prob_de_clase;
+    double max_prob = -1e40;
+    int i, k, clase_MAP;
 
-	for (k = 0; k < N_Class; k++) {
-		prob_de_clase = 0.;
+    for (k = 0; k < N_Class; k++) {
+        prob_de_clase = 0.;
 
-		/*calcula la probabilidad de cada feature individual dada la clase y la acumula */
-		for (i = 0; i < N_IN; i++)
-			prob_de_clase += log(prob(input[i], i, k));
+        /*calcula la probabilidad de cada feature individual dada la clase y la acumula */
+        for (i = 0; i < N_IN; i++)
+            prob_de_clase += log(prob(input[i], i, k));
 
-		/*agrega la probabilidad a priori de la clase */
-		 /*COMPLETAR*/ prob_de_clase += log( ...);
+        /*agrega la probabilidad a priori de la clase */
+         /*COMPLETAR*/ prob_de_clase += log( ...);
 
-		/*guarda la clase con prob maxima */
-		if (prob_de_clase >= max_prob) {
-			max_prob = prob_de_clase;
-			clase_MAP = k;
-		}
-	}
+        /*guarda la clase con prob maxima */
+        if (prob_de_clase >= max_prob) {
+            max_prob = prob_de_clase;
+            clase_MAP = k;
+        }
+    }
 
-	return clase_MAP;
+    return clase_MAP;
 }
 
 /* ------------------------------------------------------------------------------ */
@@ -299,34 +299,34 @@ int output(double *input)
 double propagar(double **S, int pat_ini, int pat_fin, int usar_seq)
 {
 
-	double mse = 0.0;
-	int nu;
-	int patron;
+    double mse = 0.0;
+    int nu;
+    int patron;
 
-	for (patron = pat_ini; patron < pat_fin; patron++) {
+    for (patron = pat_ini; patron < pat_fin; patron++) {
 
-		/*nu tiene el numero del patron que se va a presentar */
-		if (usar_seq)
-			nu = seq[patron];
-		else
-			nu = patron;
+        /*nu tiene el numero del patron que se va a presentar */
+        if (usar_seq)
+            nu = seq[patron];
+        else
+            nu = patron;
 
-		/*clase MAP para el patron nu */
-		pred[nu] = output(S[nu]);
+        /*clase MAP para el patron nu */
+        pred[nu] = output(S[nu]);
 
-		/*actualizar error */
-		if (S[nu][N_IN] != (double)pred[nu])
-			mse += 1.;
-	}
+        /*actualizar error */
+        if (S[nu][N_IN] != (double)pred[nu])
+            mse += 1.;
+    }
 
-	mse /= ((double)(pat_fin - pat_ini));
+    mse /= ((double)(pat_fin - pat_ini));
 
-	if (CONTROL > 3) {
-		printf("End prop\n");
-		fflush(NULL);
-	}
+    if (CONTROL > 3) {
+        printf("End prop\n");
+        fflush(NULL);
+    }
 
-	return mse;
+    return mse;
 }
 
 /* --------------------------------------------------------------------------------------- */
@@ -337,63 +337,63 @@ double propagar(double **S, int pat_ini, int pat_fin, int usar_seq)
 int train(char *filename)
 {
 
-	int i, j, k, feature, clase;
-	double sigma, me;
-	double train_error, valid_error, test_error;
-	FILE *salida, *fpredic;
+    int i, j, k, feature, clase;
+    double sigma, me;
+    double train_error, valid_error, test_error;
+    FILE *salida, *fpredic;
 
-	/*Asigno todos los patrones del .data como entrenamiento porque este metodo no requiere validacion */
-	N_TOTAL = PTOT;
-	/*N_TOTAL=PR; si hay validacion */
-	for (k = 0; k < PTOT; k++)
-		seq[k] = k;	/* inicializacion del indice de acceso a los datos */
+    /*Asigno todos los patrones del .data como entrenamiento porque este metodo no requiere validacion */
+    N_TOTAL = PTOT;
+    /*N_TOTAL=PR; si hay validacion */
+    for (k = 0; k < PTOT; k++)
+        seq[k] = k; /* inicializacion del indice de acceso a los datos */
 
-	/*efectuar shuffle inicial de los datos de entrenamiento si SEED != -1 (y hay validacion) */
-	if (SEED > -1 && N_TOTAL < PTOT) {
-		srand((unsigned)SEED);
-		shuffle(PTOT);
-	}
+    /*efectuar shuffle inicial de los datos de entrenamiento si SEED != -1 (y hay validacion) */
+    if (SEED > -1 && N_TOTAL < PTOT) {
+        srand((unsigned)SEED);
+        shuffle(PTOT);
+    }
 
-	/*Calcular probabilidad intrinseca de cada clase */
+    /*Calcular probabilidad intrinseca de cada clase */
 
-	/*Calcular media y desv.est. por clase y cada atributo */
+    /*Calcular media y desv.est. por clase y cada atributo */
 
-	/*calcular error de entrenamiento */
-	train_error = propagar(data, 0, PR, 1);
-	/*calcular error de validacion; si no hay, usar mse_train */
-	if (PR == PTOT)
-		valid_error = train_error;
-	else
-		valid_error = propagar(data, PR, PTOT, 1);
-	/*calcular error de test (si hay) */
-	if (PTEST > 0)
-		test_error = propagar(test, 0, PTEST, 0);
-	else
-		test_error = 0.;
-	/*mostrar errores */
-	printf("\nFin del entrenamiento.\n\n");
-	printf("Errores:\nEntrenamiento:%f%%\n", train_error * 100.);
-	printf("Validacion:%f%%\nTest:%f%%\n", valid_error * 100.,
-	       test_error * 100.);
-	if (CONTROL)
-		fflush(NULL);
+    /*calcular error de entrenamiento */
+    train_error = propagar(data, 0, PR, 1);
+    /*calcular error de validacion; si no hay, usar mse_train */
+    if (PR == PTOT)
+        valid_error = train_error;
+    else
+        valid_error = propagar(data, PR, PTOT, 1);
+    /*calcular error de test (si hay) */
+    if (PTEST > 0)
+        test_error = propagar(test, 0, PTEST, 0);
+    else
+        test_error = 0.;
+    /*mostrar errores */
+    printf("\nFin del entrenamiento.\n\n");
+    printf("Errores:\nEntrenamiento:%f%%\n", train_error * 100.);
+    printf("Validacion:%f%%\nTest:%f%%\n", valid_error * 100.,
+           test_error * 100.);
+    if (CONTROL)
+        fflush(NULL);
 
-	/* archivo de predicciones */
-	sprintf(filepat, "%s.predic", filename);
-	fpredic = fopen(filepat, "w");
-	error = (fpredic == NULL);
-	if (error) {
-		printf("Error al abrir archivo para guardar predicciones\n");
-		return 1;
-	}
-	for (k = 0; k < PTEST; k++) {
-		for (i = 0; i < N_IN; i++)
-			fprintf(fpredic, "%f\t", test[k][i]);
-		fprintf(fpredic, "%d\n", pred[k]);
-	}
-	fclose(fpredic);
+    /* archivo de predicciones */
+    sprintf(filepat, "%s.predic", filename);
+    fpredic = fopen(filepat, "w");
+    error = (fpredic == NULL);
+    if (error) {
+        printf("Error al abrir archivo para guardar predicciones\n");
+        return 1;
+    }
+    for (k = 0; k < PTEST; k++) {
+        for (i = 0; i < N_IN; i++)
+            fprintf(fpredic, "%f\t", test[k][i]);
+        fprintf(fpredic, "%d\n", pred[k]);
+    }
+    fclose(fpredic);
 
-	return 0;
+    return 0;
 }
 
 /* ----------------------------------------------------------------------------------------------------- */
@@ -401,34 +401,34 @@ int train(char *filename)
 int main(int argc, char **argv)
 {
 
-	if (argc != 2) {
-		printf
-		    ("Modo de uso: nb <filename>\ndonde filename es el nombre del archivo (sin extension)\n");
-		return 0;
-	}
+    if (argc != 2) {
+        printf
+            ("Modo de uso: nb <filename>\ndonde filename es el nombre del archivo (sin extension)\n");
+        return 0;
+    }
 
-	/* defino la estructura */
-	error = arquitec(argv[1]);
-	if (error) {
-		printf("Error en la definicion de parametros\n");
-		return 1;
-	}
+    /* defino la estructura */
+    error = arquitec(argv[1]);
+    if (error) {
+        printf("Error en la definicion de parametros\n");
+        return 1;
+    }
 
-	/* leo los datos */
-	error = read_data(argv[1]);
-	if (error) {
-		printf("Error en la lectura de datos\n");
-		return 1;
-	}
+    /* leo los datos */
+    error = read_data(argv[1]);
+    if (error) {
+        printf("Error en la lectura de datos\n");
+        return 1;
+    }
 
-	/* ajusto los parametros y calcula errores en ajuste y test */
-	error = train(argv[1]);
-	if (error) {
-		printf("Error en el ajuste\n");
-		return 1;
-	}
+    /* ajusto los parametros y calcula errores en ajuste y test */
+    error = train(argv[1]);
+    if (error) {
+        printf("Error en el ajuste\n");
+        return 1;
+    }
 
-	return 0;
+    return 0;
 
 }
 

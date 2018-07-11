304d6be554bbd3efc55c6e507b38c72c98fcfcb8
Hernán Gurmendi <hgurmen@gmail.com>
2018-07-11 18:40:08 -0300

    Removed names file for heladas dataset. Added test dataset. Added K-Group Folding.

--- ml05/resources/svm.py
+++ ml05/resources/svm.py
@@ -1,25 +1,90 @@
 #!/usr/bin/python3
 
-FOLDS=10
-INPUTS='heladas.data'
-
 import csv
+import argparse
 from sklearn import svm
+from sklearn.model_selection import GroupKFold
+from sklearn.utils import shuffle
+
+##########################################################################
+# Argument parsing
+##########################################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="input file", default='test.data')
+parser.add_argument("-f", "--folds", help="number of folds", default=10)
+parser.add_argument("-v", "--verbosity", help="verbosity level, 0 for no messages, 1 for messages", default=0)
+args = parser.parse_args()
+
+INPUT = args.input
+FOLDS = int(args.folds)
+VERBOSITY = int(args.verbosity)
+
+#quit()
+
+##########################################################################
+# Input parsing
+##########################################################################
 
 inputs = []
 classes = []
 
-with open(INPUTS, newline='') as f:
+with open(args.input, newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         inputs.append(list(map(lambda x : float(x), row[:-1])))
         classes.append(int(row[-1]))
 
-#for i in range(len(inputs)):
-#    print(inputs[i], classes[i])
+# Shuffle inputs:
+inputsShuf, classesShuf = shuffle(inputs, classes)
 
-clf = svm.SVC()
-clf.fit(inputs, classes)
-print(clf)
+if VERBOSITY > 0:
+    for i in range(len(classesShuf)):
+        print(inputsShuf[i], classesShuf[i])
+
+##########################################################################
+# K-Folds
+##########################################################################
+
+def getSplits(i):
+    foldLen = len(inputsShuf) // FOLDS
+
+    l, r = i * foldLen, (i + 1) * foldLen
+
+    inputsValid = inputsShuf[l:r]
+    classesValid = classesShuf[l:r]
+    
+    inputsTrain = inputsShuf[:]
+    del inputsTrain[l:r]
+
+    classesTrain = classesShuf[:]
+    del classesTrain[l:r]
+
+    return inputsValid, classesValid, inputsTrain, classesTrain
+
+if VERBOSITY > 0:
+    for i in range(FOLDS):
+        print('******************************')
+        print('Fold', i + 1)
+        print('******************************')
+    
+        a, b, c, d = getSplits(i)
+    
+        print('Validation:')
+        for j in range(len(a)):
+            print(a[j], b[j])
+    
+        print('Train:')
+        for j in range(len(c)):
+            print(c[j], d[j])
+
+
+##########################################################################
+# SVM fitting
+##########################################################################
+
+#clf = svm.SVC(kernel='linear')
+#clf.fit(inputs, classes)
+#print(clf)
 
 

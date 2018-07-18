6d4ddc01a5a00f9551f5c0588ceaf7d6b67934ab
Hernán Gurmendi <hgurmen@gmail.com>
2018-07-18 20:00:56 -0300

    SVM implementation now optimizes the C parameter

--- ml05/resources/svm.py
+++ ml05/resources/svm.py
@@ -14,13 +14,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', help='input files\' stem (without the .data or .test suffix)', required=True)
 parser.add_argument('-v', '--verbose', help='shows detailed output', default=False, action='store_true')
 parser.add_argument('-k', '--kernel', help='kernel type (default \'linear\')', default='linear')
-parser.add_argument('-C', help='penalty parameter C of the error term (default 1.0)', default=1.0)
+parser.add_argument('-C', nargs='*', help='penalty parameter C of the error term (default 1.0), it can be a list of different C values, in which case it will output the errors for the one with minimum training error', default=['1.0'])
 args = parser.parse_args()
 
 INPUT = args.input
 VERBOSE = args.verbose
 KERNEL = args.kernel
-PARAM_C = float(args.C)
+PARAM_C = list(map(lambda x : float(x), args.C))
 
 print('Running Support Vector Machine with the following parameters:')
 print('Input stem:', INPUT)
@@ -33,7 +33,7 @@ print('')
 # Input parsing
 ##########################################################################
 
-def readFile(path):
+def parseFile(path):
     """Reads the contents of the CSV file in path and returns two lists consisting of its inputs and classes, respectively."""
     inputs = []
     classes = []
@@ -50,7 +50,7 @@ trainFile = INPUT + '.data'
 testFile = INPUT + '.test'
 
 print('Reading train set from', trainFile)
-trainInputs, trainClasses = readFile(trainFile)
+trainInputs, trainClasses = parseFile(trainFile)
 if VERBOSE:
     print('Train file contents:')
     
@@ -58,7 +58,7 @@ if VERBOSE:
         print(trainInputs[i], trainClasses[i])
 
 print('Reading test set from', testFile)
-testInputs, testClasses = readFile(testFile)
+testInputs, testClasses = parseFile(testFile)
 if VERBOSE:
     print('Test file contents:')
     
@@ -69,27 +69,37 @@ if VERBOSE:
 # SVM fitting & error computation
 ##########################################################################
 
-clf = svm.SVC(kernel=KERNEL, C=PARAM_C)
+trainMinError, trainMaxError = 1e10, 1e10
+minC = None
 
-clf.fit(trainInputs, trainClasses)
+for c in PARAM_C:
+    clf = svm.SVC(kernel=KERNEL, C=c)
+    
+    clf.fit(trainInputs, trainClasses)
+    
+    trainPredics = clf.predict(trainInputs)
+    testPredics = clf.predict(testInputs)
+    
+    trainMisses, testMisses = 0, 0
+    
+    for i in range(len(trainPredics)):
+        if trainPredics[i] != trainClasses[i]:
+            trainMisses = trainMisses + 1
+    
+    for i in range(len(testPredics)):
+        if testPredics[i] != testClasses[i]:
+            testMisses = testMisses + 1
+    
+    trainError = trainMisses / len(trainClasses)
+    testError = testMisses / len(testClasses)
 
-trainPredics = clf.predict(trainInputs)
-testPredics = clf.predict(testInputs)
+    if trainError < trainMinError:
+        trainMinError = trainError
+        testMinError = testError
+        minC = c
 
-trainMisses, testMisses = 0, 0
-
-for i in range(len(trainPredics)):
-    if trainPredics[i] != trainClasses[i]:
-        trainMisses = trainMisses + 1
-
-for i in range(len(testPredics)):
-    if testPredics[i] != testClasses[i]:
-        testMisses = testMisses + 1
-
-trainError = trainMisses / len(trainClasses)
-testError = testMisses / len(testClasses)
-
-print('TRAIN ERROR:', trainError)
-print('TEST ERROR:', testError)
+print('C:', minC)
+print('TRAIN ERROR:', trainMinError * 100)
+print('TEST ERROR:', testMinError * 100)
 print('')
 
